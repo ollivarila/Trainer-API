1e0f6449730650fe3a1c335511e7837f09ca4f60
ollivarila <olli.varila@gmail.com>
2023-04-02 18:51:54 +0300

    Fix

--- src/main/java/com/example/trainerapi/TrainerApiApplication.java
+++ src/main/java/com/example/trainerapi/TrainerApiApplication.java
@@ -1,19 +1,13 @@
 package com.example.trainerapi;
 
-import com.example.trainerapi.models.entities.User;
-import com.example.trainerapi.models.entities.Workout;
-import com.example.trainerapi.models.repositories.UserRepository;
-import com.example.trainerapi.models.repositories.WorkoutRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 
-// todo remove all useless log statements from everywhere
 /**
  * Entrypoint for the application
  */
@@ -24,15 +18,10 @@ public class TrainerApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrainerApiApplication.class, args);
 	}
-	private final WorkoutRepository workoutRepository;
 
-	@Autowired
-	public TrainerApiApplication(WorkoutRepository workoutRepository) {
-		this.workoutRepository = workoutRepository;
-	}
 	/**
 	 * Method for debugging
-	 * @return Commandlinerunner that gets executed on startup
+	 * @return CommandLineRunner that gets executed on startup
 	 */
 	public CommandLineRunner demo() {
 		return (args) -> {
