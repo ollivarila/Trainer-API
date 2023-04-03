476ea22843bac883bfd41a6a22a7edf0b2b077f9
ollivarila <olli.varila@gmail.com>
2023-04-03 19:51:06 +0300

    Added default ExerciseTypes on user creation and some fixes.

---
+++ src/main/java/com/example/trainerapi/security/util/UserCreationUtil.java
@@ -0,0 +1,32 @@
+package com.example.trainerapi.security.util;
+
+import com.example.trainerapi.models.entities.ExerciseType;
+import com.example.trainerapi.models.entities.User;
+import com.example.trainerapi.models.repositories.ExerciseTypeRepository;
+import com.example.trainerapi.models.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCreationUtil {
+
+    private final ExerciseTypeRepository exerciseTypeRepository;
+    private static String[] basicExercises = {
+            "squat", "front squat", "bench press", "incline bench press",
+            "dumbbell press", "deadlift", "romanian deadlift", "barbell row",
+            "overhead press", "barbell curl", "dumbbell curl", "tricep extension"
+    };
+    @Autowired
+    public UserCreationUtil(ExerciseTypeRepository exerciseTypeRepository) {
+        this.exerciseTypeRepository = exerciseTypeRepository;
+    }
+
+    public void createDefaultExerciseTypes(User user){
+        for(String exerciseName : basicExercises){
+            ExerciseType exerciseType = new ExerciseType(exerciseName);
+            exerciseType.setUser(user);
+            exerciseTypeRepository.save(exerciseType);
+        }
+
+    }
+}
